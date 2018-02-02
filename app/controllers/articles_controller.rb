7bcdf2e6cde6e60919f4e01d470c253f0b8819dd
baixiaoji <1041874421@qq.com>
2018-02-02 15:18:10 +0800

    [feat] 增加了文章的update的功能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,29 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
     def show
         @article = Article.find(params[:id])
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
     def new
         @article = Article.new
     end
+
     def create
         @article = Article.new(article_params)
 
